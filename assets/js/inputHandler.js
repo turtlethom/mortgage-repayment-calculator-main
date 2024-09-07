e9aea4d8d5cd978dcb36d6023d8251dcd4aa1247
Thomas James <wjamesthomas3@gmail.com>
2024-09-07 17:08:49 -0400

    Added Focus States For Text Inputs

--- assets/js/inputHandler.js
+++ assets/js/inputHandler.js
@@ -1,8 +1,31 @@
 function handleSymbols() {
+    const amountInput = document.getElementById('mortgage-amount');
     const sterlingSymbol = document.getElementById('sterling');
-    const termUnit = document.getElementById('term-unit');
+
+    const termInput = document.getElementById('mortgage-term');
+    const termSymbol = document.getElementById('term-symbol');
+
+    const interestRateInput = document.getElementById('interest-rate');
     const percentageSymbol = document.getElementById('percentage-unit');
 
+    amountInput.addEventListener('focus', () => handleFocus(amountInput, sterlingSymbol));
+    termInput.addEventListener('focus', () => handleFocus(termInput, termSymbol));
+    interestRateInput.addEventListener('focus', () => handleFocus(interestRateInput, percentageSymbol));
+
+    amountInput.addEventListener('focusout', () => handleFocusOut(amountInput, sterlingSymbol));
+    termInput.addEventListener('focusout', () => handleFocusOut(termInput, termSymbol));
+    interestRateInput.addEventListener('focusout', () => handleFocusOut(interestRateInput, percentageSymbol));
+
+}
+
+function handleFocus(input, symbol) {
+    if (document.activeElement === input) {
+        symbol.classList.add('symbol-active');
+    }
+}
+
+function handleFocusOut(input, symbol) {
+    symbol.classList.remove('symbol-active');
 }
 
 export default handleSymbols;
